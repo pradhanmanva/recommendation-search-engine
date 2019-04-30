b52fc743595f153ce1e1529d76100ce489077ec6
Manva Pradhan <pradhanmanva@gmail.com>
2019-04-30 17:14:06 -0500

    Update recommendation-search-engine.R

--- recommendation-search-engine.R
+++ recommendation-search-engine.R
@@ -9,10 +9,11 @@ sqlContext <- sparkRSQL.init(sc)
 
 #setting up file paths
 data_file <- "/Users/pradhanmanva/Downloads/R for DS/Project2/recommendation-search-engine/input/plot_summaries.txt"
-saved_data_file <- "/Users/pradhanmanva/Downloads/R for DS/Project2/recommendation-search-engine/plot_summary.RData "
+vector_file <- "/Users/pradhanmanva/Downloads/R for DS/Project2/recommendation-search-engine/plot_summary.RData"
+corpus_file <- "/Users/pradhanmanva/Downloads/R for DS/Project2/recommendation-search-engine/summary_corpus.RData"
 
 #read the data from "plot_summaries.txt" and save the data into a file
-if (file.exists(saved_data_file)){
+if (file.exists(vector_file)){
   plot_summary <- read.delim(data_file, header=FALSE, sep = "\t", quote="")
   plot_summary$V2 <- as.character(plot_summary$V2)
   saveRDS(plot_summary, "plot_summary.RData")
@@ -20,18 +21,27 @@ if (file.exists(saved_data_file)){
 #reading the saved data.
 plot_summary <- readRDS("plot_summary.RData")
 
-#getting the plot summaries into another dataframe
-summary_data <- plot_summary$V2
-
-#creating VCorpus for stemming by removing punctuations, stopwords and numbers, and strip whitespaces.
-addspace <- content_transformer(function(x, pattern) {
-  return(gsub(pattern, " ", x))
-})
-summary_corpus <- VCorpus(VectorSource(summary_data))
-summary_corpus <- tm_map(summary_corpus, addspace, "-")
-summary_corpus <- tm_map(summary_corpus, addspace, "'")
-summary_corpus <- tm_map(summary_corpus, removePunctuation)
-summary_corpus <- tm_map(summary_corpus, removeNumbers)
-summary_corpus <- tm_map(summary_corpus, removeWords, stopwords("english"))
-summary_corpus <- tm_map(summary_corpus, content_transformer(tolower))
-summary_corpus <- tm_map(summary_corpus, stripWhitespace)
+if(!file.exists(corpus_file)){
+  #getting the plot summaries into another dataframe
+  summary_data <- plot_summary$V2
+  
+  #creating VCorpus for stemming by removing punctuations, stopwords and numbers, and strip whitespaces.
+  addspace <- content_transformer(function(x, pattern) {
+    return(gsub(pattern, " ", x))
+  })
+  summary_corpus <- VCorpus(VectorSource(summary_data))
+  summary_corpus <- tm_map(summary_corpus, addspace, "-")
+  summary_corpus <- tm_map(summary_corpus, addspace, "'")
+  summary_corpus <- tm_map(summary_corpus, removePunctuation)
+  summary_corpus <- tm_map(summary_corpus, removeNumbers)
+  summary_corpus <- tm_map(summary_corpus, removeWords, stopwords("english"))
+  summary_corpus <- tm_map(summary_corpus, content_transformer(tolower))
+  summary_corpus <- tm_map(summary_corpus, stripWhitespace)
+  
+  #stemming the documents
+  summary_corpus <- tm_map(summary_corpus, stemDocument)
+  summary_dtm <- DocumentTermMatrix(summary_corpus, control = list(weighting = function(x) weightTfIdf(x, normalize = FALSE),
+    stopwords = TRUE))
+  saveRDS(summary_dtm, "summary_corpus.RData")
+}
+summary_dtm <- readRDS("summary_corpus.RData")
