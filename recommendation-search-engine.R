efb3e76432319bd49019684a983edd19af054000
Manva Pradhan <pradhanmanva@gmail.com>
2019-05-02 08:19:39 -0500

    Update recommendation-search-engine.R

--- recommendation-search-engine.R
+++ recommendation-search-engine.R
@@ -13,16 +13,17 @@ sqlContext <- sparkRSQL.init(sc)
 data_file <- "input/plot_summaries.txt"
 vector_file <- "plot_summary.RData"
 corpus_file <- "summary_corpus.RData"
+dtm_df_file <- "summary_tdidf_df.RData"
 
 #read the data from "plot_summaries.txt" and save the data into a file
 if (!file.exists(vector_file)){
   plot_summary <- read.delim(data_file, header=FALSE, sep = "\t", quote="")
   plot_summary$V2 <- as.character(plot_summary$V2)
-  saveRDS(plot_summary, "plot_summary.RData")
+  saveRDS(plot_summary, vector_file)
   print("Saved!")
 }
 #reading the saved data.
-plot_summary <- readRDS("plot_summary.RData")
+plot_summary <- readRDS(vector_file)
 
 if(!file.exists(corpus_file)){
   #getting the plot summaries into another dataframe
@@ -35,20 +36,24 @@ if(!file.exists(corpus_file)){
   summary_corpus <- VCorpus(VectorSource(summary_data))
   summary_corpus <- tm_map(summary_corpus, removePunctuation)
   summary_corpus <- tm_map(summary_corpus, removeNumbers)
-  summary_corpus <- tm_map(summary_corpus, removeWords, stopwords("english"))
   summary_corpus <- tm_map(summary_corpus, content_transformer(tolower))
+  summary_corpus <- tm_map(summary_corpus, removeWords, stopwords("english"))
   summary_corpus <- tm_map(summary_corpus, stripWhitespace)
   
   #stemming the documents
   summary_corpus <- tm_map(summary_corpus, stemDocument)
   summary_dtm <- DocumentTermMatrix(summary_corpus, control = list(weighting = function(x) weightTfIdf(x, normalize = FALSE), stopwords = TRUE))
-  saveRDS(summary_dtm, "summary_corpus.RData")
+  saveRDS(summary_dtm, corpus_file)
   print("Saved!")
 }
-summary_dtm <- readRDS("summary_corpus.RData")
+summary_dtm <- readRDS(corpus_file)
 
-# summary_dtm$dimnames$Docs <- as.numeric(summary_dtm$dimnames$Docs)
-# summary_dtm$dimnames$Terms <- as.character(summary_dtm$dimnames$Terms)
-# summary_dtm$dimnames$Terms <- gsub("[^[:alpha:] ]","", summary_dtm$dimnames$Terms)
-
+if(!file.exists(dtm_df_file)){
+  #converting the DTM to dataframe and manipulating it to get the TD_IDF values in it.
+  summary_dtm_table <- tidy(summary_dtm)
+  summary_dtm_table <- bind_tf_idf(summary_dtm_table, term = term, document = document, n=count)
+  saveRDS(summary_dtm_table, dtm_df_file)
+  print("Saved!")
+}
+plot_tdidf <- readRDS(dtm_df_file)
 
